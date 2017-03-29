43c59db274f2d3b13e23369a3ba677b504b148c2
Satyapriya Baral <satyapriyab@mindfiresolutions.com>
2017-03-29 17:50:40 +0530

    Add Post functionality done

--- KisanSeva/public/bower_components/AdminLTE/dist/js/script.js
+++ KisanSeva/public/bower_components/AdminLTE/dist/js/script.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-      $("#selectCategory").on("change",function(){
-         console.log("changing");
-           var cat_id = $(this).val();
-     //      var url = "{{ route('viewCrops')}}";
-           console.log(cat_id);
-           $.ajax({
-             type:'get',
-             url: url ,
-             data:{'id':cat_id},
-             success:function(response){
-                 console.log(response);
-                 //console.log(data);
-             }
-           })
-      });
-
- });
+  $("#selectCategory").on("change",function(){
+    var cat_id = $(this).val();
+    var div=$(this).parent().parent();
+    var op=" ";
+    $.ajax({
+      type:'get',
+      url: url ,
+      data:{'id':cat_id},
+      success:function(data){
+        console.log(data);
+        op+='<option value="0" selected disabled> Choose Crop </option>';
+        for( var i=0 ; i < data.length ; i++ ) {
+          op+='<option value="'+data[i][1]+'">'+data[i][0]+'</option>';
+        }
+        div.find('.cropName').html(" ");
+        div.find('.cropName').append(op);
+      }
+    })
+  });
+});
